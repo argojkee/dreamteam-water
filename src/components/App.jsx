3573ce1614a6480eb648d7f7b38c3ade183f0919
ExLuZiVe53 <imstudent27@gmail.com>
2024-01-27 14:15:05 +0100

    write style Main-DailyNorma

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import { getToken } from '../redux/auth/authSelectors';
 const HomePage = lazy(() => import('../pages/HomePage'));
 const RegistrationPage = lazy(() => import('../pages/RegistrationPage'));
 const LoginPage = lazy(() => import('../pages/LoginPage'));
-const MainPage = lazy(() => import('../pages/MainPage'));
+const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
---
+++ src/pages/MainPage/MainPage.styled.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const StyledMainPage = styled.section`
+  max-width: 280px;
+  width: 300px;
+  height: 208px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  .dailyNormaSection {
+    width: 164px;
+    height: 174px;
+
+    padding: 8px 20px 8px 20px;
+
+    background-color: linear-gradient(0deg, #ffffff, #ffffff),
+      linear-gradient(0deg, #ecf2ff, #ecf2ff);
+    border: 1px solid rgba(236, 242, 255, 1);
+    border-radius: 10px;
+  }
+
+  h2 {
+    font-family: Roboto;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.33;
+
+    text-align: left;
+  }
+
+  span {
+    font-family: Roboto;
+    font-size: 22px;
+    font-weight: 700;
+    line-height: 1;
+
+    text-align: left;
+
+    color: rgba(64, 123, 255, 1);
+  }
+
+  button {
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.25;
+
+    border: none;
+    background-color: inherit;
+
+    /* width: 28px;
+    height: 20px; */
+
+    text-align: left;
+
+    color: rgba(139, 174, 255, 1);
+  }
+
+  /* background-image: url('../../images/main-bg/Bottle-Main-sm.png'); */
+`;

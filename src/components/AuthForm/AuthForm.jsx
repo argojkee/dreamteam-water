1652eb713ca45362fc7ada997d8fbd962044d044
DmShko <vombat_schev@i.ua>
2024-01-27 22:56:58 +0200

    signIn-signUp-04

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { useFormik } from "formik";
 import * as Yup from 'yup';
@@ -7,9 +7,14 @@ import * as Yup from 'yup';
 import signInAPI from '../../API/Auth/signInAPI';
 import signUpAPI from '../../API/Auth/signUpAPI';
 
+/* styles import */
+import Styles from './Styles';
+/* end */
+
 const AuthForm = () => {
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   // get current location
   const location = useLocation();
@@ -20,26 +25,19 @@ const AuthForm = () => {
   // But we have two variants form (logIn and register).
   // We must create own validate rules function, because 
   // validation expression must be different for each situation.
-  const validationSchemaBody = () => {
-    if(!isRegistrationPage) {
-      return {
-        email: Yup.string().matches(
-          /\w{0}[a-zA-Zа-яА-Я]+\@\w{0}[a-zA-Zа-яА-Я]+\.\w{0}[a-zA-Zа-яА-Я]/,
-          { message: 'Invalid email'}
-        ).required("'Email' field is required"),
-        password: Yup.string().min(8, 'Must be 8 characters or more').required("'Password' field is required"),
-      }
-    }else {
-      return {
-        email: Yup.string().matches(
-          /\w{0}[a-zA-Zа-яА-Я]+\@\w{0}[a-zA-Zа-яА-Я]+\.\w{0}[a-zA-Zа-яА-Я]/,
-          { message: 'Invalid email' }
-        ).required("'Email' field is required"),
-        password: Yup.string().min(8, 'Must be 8 characters or more').required("'Password' field is required"),
-        repeatPassword: Yup.string().min(8, 'Must be 8 characters or more').required("'RepeatPassword' field is required")
-        .oneOf([Yup.ref('password'), null], 'Passwords must match'), 
-      }
-    };
+ 
+  const validationLoginForm = {
+    email: Yup.string().matches(
+      /\w{0}[a-zA-Zа-яА-Я]+\@\w{0}[a-zA-Zа-яА-Я]+\.\w{0}[a-zA-Zа-яА-Я]/,
+      { message: 'Invalid email'}
+    ).required("'Email' field is required"),
+    password: Yup.string().min(8, 'Must be 8 characters or more').required("'Password' field is required"),
+  };
+
+  const validationRegisterForm = {
+    ...validationLoginForm,
+    repeatPassword: Yup.string().min(8, 'Must be 8 characters or more').required("'RepeatPassword' field is required")
+    .oneOf([Yup.ref('password'), null], 'Passwords must match'), 
   };
 
   // create 'formik' hook and configurate him
@@ -51,7 +49,7 @@ const AuthForm = () => {
     },
 
     //yup stored own validate functions (for email, password...etc)
-    validationSchema: Yup.object(validationSchemaBody()),
+    validationSchema: Yup.object(isRegistrationPage ?  validationRegisterForm : validationLoginForm),
     
     //!'values' contains ended values all Form inputs. They will can get: 'values.<field name>' 
     onSubmit: values => {
@@ -65,55 +63,70 @@ const AuthForm = () => {
 
   });
 
+  const navTo = () => {
+    isRegistrationPage ? navigate('/login') : navigate('/registration');
+  };
+
   return (
-    <div>
-        <form onSubmit={formik.handleSubmit}>
+   
+       <Styles $div $justify={'center'}>
 
-            <label htmlFor="email">Enter your email</label>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.email}
-            />
+          <Styles $div $divDiraction={'column'} height={'312px'} width={'384px'}>
 
-            <label htmlFor="password">Enter your password</label>
-              <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.password}
-            />
+            <Styles $p $fontSize={'26px'} $marginBott={'16px'}>Sign In</Styles>
 
-            {isRegistrationPage  && 
-               <>
-                  <label htmlFor="repeatPassword">Repeat password</label>
-                  <input
-                      id="repeatPassword"
-                      name="repeatPassword"
-                      type="password"
+            <Styles onSubmit={formik.handleSubmit} $form $formDiraction={'column'}>
+
+            <Styles $label htmlFor="email"><Styles $p $fontWeight={'400'}>Enter your email</Styles></Styles>
+                <Styles $input $borderRadius={'6px'} $marginBott={'16px'}
+                  
+                      id="email"
+                      name="email"
+                      type="email"
                       onChange={formik.handleChange}
                       onBlur={formik.handleBlur}
-                      value={formik.values.repeatPassword}>
-                    
-                  </input>
-                </>
-            }
-          
-            <div style={{color: 'orange',}}>
-                {formik.touched.email && formik.errors.email ? formik.errors.email 
-                : formik.touched.password && formik.errors.password ? formik.errors.password
-                : formik.touched.repeatPassword && formik.errors.repeatPassword ? formik.errors.repeatPassword : ''}
-            </div>
+                      value={formik.values.email}
+                  
+                />
 
-            <button type="submit">Submit</button>
+              <Styles $label htmlFor="password"><Styles $p $fontWeight={'400'}>Enter your password</Styles></Styles>
+              <Styles $input $borderRadius={'6px'}
+                    id="password"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+              />
 
-        </form>
-    </div>
+              {isRegistrationPage  && 
+                <>
+                    <Styles $label htmlFor="repeatPassword"><Styles $p $fontWeight={'400'}>Repeat password</Styles></Styles>
+                    <Styles $input $borderRadius={'6px'}
+                        id="repeatPassword"
+                        name="repeatPassword"
+                        type="password"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        value={formik.values.repeatPassword}
+                    />
+                  </>
+              }
+            
+              <Styles $div color={'#EF5050'} height={'8px'}>
+                  {formik.touched.email && formik.errors.email ? formik.errors.email 
+                  : formik.touched.password && formik.errors.password ? formik.errors.password
+                  : formik.touched.repeatPassword && formik.errors.repeatPassword ? formik.errors.repeatPassword : ''}
+              </Styles>
+
+              <Styles $button type="submit" $borderRadius={'10px'}>Submit</Styles>
+            </Styles>
+
+            <Styles $link onClick={navTo}> To {isRegistrationPage ? 'login' : 'registration'} page{' '}</Styles>
+
+        </Styles>
+        
+      </Styles>
   )
 }
 

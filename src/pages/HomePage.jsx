94cebf8006a76a1d79e7769aaab054cadfb2114d
Aleksandr <aik5787@gmail.com>
2024-01-27 17:39:42 -0500

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,36 @@
-
+import { BackgroundContainer} from './HomePage.styled';
+import settingIcon from '../homepageimages/Setting.svg'; 
+import StatisticsIcon from '../homepageimages/Statistics.svg'; 
+import CalendarIcon from '../homepageimages/Calendar.svg'; 
 
 const HomePage = () => {
-  
-    return (
-      <>
-      </>
-     
-     
-    );
+  return (
+    <>
+      <BackgroundContainer />
+      <div>
+        <div>
+        <h1>Water consumption tracker</h1>
+        <p>Record daily water intake and track</p>
+<ul>
+  <li>
+  <img src={CalendarIcon} alt="CalendarIcon" width={40} height={40} />
+    <p>Habit drive</p>
+  </li>
+  <li>
+  <img src={StatisticsIcon} alt="StatisticsIcon" width={40} height={40} />
+    <p>View statistics</p>
+  </li>
+  <li>
+  <img src={settingIcon} alt="Setting Icon" width={40} height={40} />
+    <p>Personal rate setting</p>
+  </li>
+</ul>
+      <button>Try tracker</button>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
--- src/pages/HomePage.styled.jsx
+++ src/pages/HomePage.styled.jsx
@@ -1,19 +1,28 @@
-// export const BackgroundContainer = styled.div`
-//   width: 100%;
-//   height: 100vh;
+import styled from 'styled-components';
+import desktop1 from '../homepageimages/Desktop1.png';
+import desktop2 from '../homepageimages/Desktop2.png';
+import tablet from '../homepageimages/Tablet.png';
+import mobile from '../homepageimages/Mobile.png';
 
-//   @media (min-width: 1440px) {
-//     background-image: url('../../public/homepageimages/1440image.png');
-//   }
+export const BackgroundContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center bottom;
+  
+  @media (max-width: 767px) {
+    background-image: url(${mobile});
+  }
 
-//   @media (max-width: 1439px) and (min-width: 768px) {
-//     background-image: url('../../public/homepageimages/1440image.png');
-//   }
+  @media (min-width: 768px) and (max-width: 1199px) {
+    background-image: url(${tablet});
+  }
 
-//   @media (max-width: 767px) {
-//     background-image: url('../../public/homepageimages/1440image.png');
-//   }
-
-//   background-size: cover;
-//   background-position: center;
-// `;
+  @media (min-width: 1200px) {
+    /* max-width: 1440px; */
+    margin: 0 auto;
+    background-image: url(${desktop2}), url(${desktop1});
+    background-position: center bottom 125px, center bottom;
+  }
+`;

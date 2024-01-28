9c3faf15e4d708540ea3b558df825f6cfe7f54aa
Matviienko Ruslana <brauniesrus@gmail.com>
2024-01-28 22:38:31 +0200

    header
    отправляю Хедер

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,8 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getToken } from '../../redux/auth/authSelectors';
 import Container from 'components/Container/Container';
-import UserOwnPopover from '../UserOwnPopover/UserOwnPopover';
+// import UserOwnPopover from '../UserOwnPopover/UserOwnPopover';
+import UserLogoModal from '../UserLogoModal/UserLogoModal';
 import { HeaderStyles } from './HeaderStyled.styled';
 import iconHome from '../../icons/Logo.png';
 import avatar from '../../icons/outline.png';
@@ -13,25 +14,29 @@ const Header = () => {
   return (
     <HeaderStyles>
       <Container>
-        <div className="containerHeader">
-          <NavLink to={token ? '/main' : '/'} className="logoBox">
-            <div>
-              <img src={iconHome} alt="iconHome" />
-            </div>
-            <div className="logoText">Tracker of water</div>
-          </NavLink>
-
-          {!token && (
-            <div className="AuthContainer">
-              <NavLink className="textSignIn" to="login">Sign in</NavLink>
+        <containerHeader>
+          <div className="containerHeader">
+            <NavLink to={token ? '/main' : '/'} className="logoBox">
               <div>
-                <img src={avatar} alt="iconHome" />
+                <img src={iconHome} alt="iconHome" />
               </div>
-            </div>
-          )}
+              <div className="logoText">TRACKER of water</div>
+            </NavLink>
 
-          {token && <UserOwnPopover />}
-        </div>
+            {!token && (
+              <div className="AuthContainer">
+                <NavLink className="textSignIn" to="login">
+                  Sign in
+                </NavLink>
+                <div>
+                  <img src={avatar} alt="iconHome" />
+                </div>
+              </div>
+            )}
+
+            {token && <UserLogoModal />}
+          </div>
+        </containerHeader>
       </Container>
     </HeaderStyles>
   );

f84f88b80dac2797ac60326f7a6850f461a62376
DmShko <vombat_schev@i.ua>
2024-01-28 20:39:02 +0200

    Update AuthForm.jsx

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -60,12 +60,17 @@ const AuthForm = () => {
       isRegistrationPage ? validationRegisterForm : validationLoginForm
     ),
 
-    //! 'values' contains ended values all Form inputs.
+    //! 'values' contains ended values all Form inputs. 
     //! They will can get: 'values.<field name>' or change values on {email, password}
-    onSubmit: ({ email, password }) => {
+    onSubmit: ({email, password}) => {
       isRegistrationPage
-        ? dispatch(signUpAPI({ email, password }))
-        : dispatch(signInAPI({ email, password }));
+           ? dispatch(
+               signUpAPI({ email, password,})
+             )
+           : dispatch(
+               signInAPI({ email, password })
+            );
+
     },
   });
   
@@ -173,8 +178,8 @@ const AuthForm = () => {
           </Styles>
         </Styles>
 
-        <Styles $link onClick={navTo}>
-          To {isRegistrationPage ? 'login' : 'registration'} page
+        <Styles $div $justify={'flex-start'} width={'100%'}>
+          <Styles $link onClick={navTo}>To {isRegistrationPage ? 'Sign in' : 'Sign up'}</Styles>
         </Styles>
       </Styles>
     </Styles>

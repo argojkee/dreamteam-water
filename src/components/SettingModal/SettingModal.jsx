1748fb857f2f0e55a18b5e2144977b36d95b2c7e
Igor Lunyakin <lunyaigor484@gmail.com>
2024-01-29 01:47:32 +0200

    add styles

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -65,178 +65,260 @@ export const SettingModal = ({ closeModal }) => {
 
   return (
     <SettingModalStyled>
-      <h2>Setting</h2>
-      <p>Your photo</p>
-      <img src="" alt="avatar" />
-      <label className="upload-photo-label">
-        <BsUpload color={iconColor} width={16} height={16} />
-        <p>Upload a photo</p>
-        <input
-          type="file"
-          name="upload_photo"
-          className="photo-input"
-          accept=".png, .jpg, .jpeg"
-        />
-      </label>
-      <form onSubmit={formik.handleSubmit}>
-        <div>
-          <p>Your gender identity</p>
-          <label>
+      <h2 className="setting-title">Setting</h2>
+      <div>
+        <p className="setting-text setting-modal-text">Your photo</p>
+        <div className="setting-photo-wrapper">
+          <img src="" alt="avatar" className="setting-avatar" />
+          <label className="upload-photo-label">
+            <BsUpload color={iconColor} />
+            <p className="upload-photo-text">Upload a photo</p>
             <input
-              type="radio"
-              name="gender"
-              value="Woman"
-              onChange={formik.handleChange}
-              checked={formik.values.gender === 'Woman'}
+              type="file"
+              name="upload_photo"
+              className="photo-input"
+              accept=".png, .jpg, .jpeg"
             />
-            <p>Woman</p>
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="Man"
-              onChange={formik.handleChange}
-              checked={formik.values.gender === 'Man'}
-            />
-            <p>Man</p>
           </label>
         </div>
-        <label>
-          <p>Your name</p>
-          <input
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.name}
-            onBlur={formik.handleBlur}
-            name="name"
-            placeholder="Name"
-          />
-          {formik.touched.repeatNewPassword && formik.errors.name && (
-            <p>{formik.errors.name}</p>
-          )}
-        </label>
-        <label>
-          <p>E-mail</p>
-          <input
-            type="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            onBlur={formik.handleBlur}
-            name="email"
-            placeholder="E-mail"
-          />
-          {formik.touched.repeatNewPassword && formik.errors.email && (
-            <p>{formik.errors.email}</p>
-          )}
-        </label>
-        <div>
-          <p>Password</p>
-          <label>
-            <p>Outdated password</p>
-            <span>
+      </div>
+      <form onSubmit={formik.handleSubmit} className="setting-form">
+        <div className="setting-form-wrapper">
+          <div className="setting-form-content-wrapper">
+            <div className="setting-form-gender-identity-wrapper">
+              <p className="setting-text setting-form-text">
+                Your gender identity
+              </p>
+              <div className="setting-form-gender-wrapper">
+                <label className="setting-form-gender-label">
+                  <input
+                    className="setting-form-gender-button"
+                    type="radio"
+                    name="gender"
+                    value="Woman"
+                    onChange={formik.handleChange}
+                    checked={formik.values.gender === 'Woman'}
+                  />
+                  <p className="setting-form-gender-text">Woman</p>
+                </label>
+                <label className="setting-form-gender-label">
+                  <input
+                    className="setting-form-gender-button"
+                    type="radio"
+                    name="gender"
+                    value="Man"
+                    onChange={formik.handleChange}
+                    checked={formik.values.gender === 'Man'}
+                  />
+                  <p className="setting-form-gender-text">Man</p>
+                </label>
+              </div>
+            </div>
+            <label className="setting-form-name-label">
+              <p className="setting-text setting-modal-text">Your name</p>
               <input
-                name="password"
-                placeholder="Password"
-                type={showPassword ? 'text' : 'password'}
-                value={formik.values.password}
+                style={
+                  formik.touched.name &&
+                  formik.errors.name && { borderColor: '#EF5050' }
+                }
+                className="setting-form-input"
+                type="text"
                 onChange={formik.handleChange}
+                value={formik.values.name}
                 onBlur={formik.handleBlur}
+                name="name"
+                placeholder="Name"
               />
-              {showPassword ? (
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  <FiEye color={iconColor} width={16} height={16} />
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  <FiEyeOff color={iconColor} width={16} height={16} />
-                </button>
+              {formik.touched.name && formik.errors.name && (
+                <p className="setting-form-input-error">{formik.errors.name}</p>
               )}
-              {formik.touched.repeatNewPassword && formik.errors.password && (
-                <p>{formik.errors.password}</p>
-              )}
-            </span>
-          </label>
-          <label>
-            <p>New Password</p>
-            <span>
+            </label>
+            <label>
+              <p className="setting-text setting-modal-text">E-mail</p>
               <input
-                type={showNewPassword ? 'text' : 'password'}
+                style={
+                  formik.touched.email &&
+                  formik.errors.email && { borderColor: '#EF5050' }
+                }
+                className="setting-form-input"
+                type="email"
                 onChange={formik.handleChange}
-                value={formik.values.newPassword}
+                value={formik.values.email}
                 onBlur={formik.handleBlur}
-                name="newPassword"
-                placeholder="Password"
+                name="email"
+                placeholder="E-mail"
               />
-              {showNewPassword ? (
-                <button
-                  type="button"
-                  onClick={() => setShowNewPassword(!showNewPassword)}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  <FiEye color={iconColor} width={16} height={16} />
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  onClick={() => setShowNewPassword(!showNewPassword)}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  <FiEyeOff color={iconColor} width={16} height={16} />
-                </button>
+              {formik.touched.email && formik.errors.email && (
+                <p className="setting-form-input-error">
+                  {formik.errors.email}
+                </p>
               )}
-              {formik.touched.repeatNewPassword &&
-                formik.errors.newPassword && <p>{formik.errors.newPassword}</p>}
-            </span>
-          </label>
-          <label>
-            <p>Repeat new password</p>
-            <span>
-              <input
-                type={showRepeatNewPassword ? 'text' : 'password'}
-                onChange={formik.handleChange}
-                value={formik.values.repeatNewPassword}
-                onBlur={formik.handleBlur}
-                name="repeatNewPassword"
-                placeholder="Password"
-              />
-              {showRepeatNewPassword ? (
-                <button
-                  type="button"
-                  onClick={() =>
-                    setShowRepeatNewPassword(!showRepeatNewPassword)
+            </label>
+          </div>
+          <div className="setting-form-password-wrapper">
+            <p className="setting-text">Password</p>
+            <label>
+              <p className="setting-modal-text setting-form-password-text">
+                Outdated password
+              </p>
+              <span className="setting-form-input-wrapper">
+                <input
+                  style={
+                    formik.touched.password &&
+                    formik.errors.password && { borderColor: '#EF5050' }
                   }
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  <FiEye color={iconColor} width={16} height={16} />
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  onClick={() =>
-                    setShowRepeatNewPassword(!showRepeatNewPassword)
-                  }
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  <FiEyeOff color={iconColor} width={16} height={16} />
-                </button>
-              )}
-              {formik.touched.repeatNewPassword &&
-                formik.errors.repeatNewPassword && (
-                  <p>{formik.errors.repeatNewPassword}</p>
+                  className="setting-form-input"
+                  name="password"
+                  placeholder="Password"
+                  type={showPassword ? 'text' : 'password'}
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {showPassword ? (
+                  <button
+                    className="setting-form-password-button"
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    <FiEye
+                      color={iconColor}
+                      className="setting-form-password-icon"
+                    />
+                  </button>
+                ) : (
+                  <button
+                    className="setting-form-password-button"
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    <FiEyeOff
+                      color={iconColor}
+                      className="setting-form-password-icon"
+                    />
+                  </button>
                 )}
-            </span>
-          </label>
+                {formik.touched.password && formik.errors.password && (
+                  <p className="setting-form-input-error">
+                    {formik.errors.password}
+                  </p>
+                )}
+              </span>
+            </label>
+            <label>
+              <p className="setting-modal-text setting-form-password-text">
+                New Password
+              </p>
+              <span className="setting-form-input-wrapper">
+                <input
+                  style={
+                    formik.touched.newPassword &&
+                    formik.errors.newPassword && { borderColor: '#EF5050' }
+                  }
+                  className="setting-form-input"
+                  type={showNewPassword ? 'text' : 'password'}
+                  onChange={formik.handleChange}
+                  value={formik.values.newPassword}
+                  onBlur={formik.handleBlur}
+                  name="newPassword"
+                  placeholder="Password"
+                />
+                {showNewPassword ? (
+                  <button
+                    className="setting-form-password-button"
+                    type="button"
+                    onClick={() => setShowNewPassword(!showNewPassword)}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    <FiEye
+                      color={iconColor}
+                      className="setting-form-password-icon"
+                    />
+                  </button>
+                ) : (
+                  <button
+                    className="setting-form-password-button"
+                    type="button"
+                    onClick={() => setShowNewPassword(!showNewPassword)}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    <FiEyeOff
+                      color={iconColor}
+                      className="setting-form-password-icon"
+                    />
+                  </button>
+                )}
+                {formik.touched.newPassword && formik.errors.newPassword && (
+                  <p className="setting-form-input-error">
+                    {formik.errors.newPassword}
+                  </p>
+                )}
+              </span>
+            </label>
+            <label>
+              <p className="setting-modal-text setting-form-password-text">
+                Repeat new password
+              </p>
+              <span className="setting-form-input-wrapper">
+                <input
+                  style={
+                    formik.touched.repeatNewPassword &&
+                    formik.errors.repeatNewPassword && {
+                      borderColor: '#EF5050',
+                    }
+                  }
+                  className="setting-form-input"
+                  type={showRepeatNewPassword ? 'text' : 'password'}
+                  onChange={formik.handleChange}
+                  value={formik.values.repeatNewPassword}
+                  onBlur={formik.handleBlur}
+                  name="repeatNewPassword"
+                  placeholder="Password"
+                />
+                {showRepeatNewPassword ? (
+                  <button
+                    className="setting-form-password-button"
+                    type="button"
+                    onClick={() =>
+                      setShowRepeatNewPassword(!showRepeatNewPassword)
+                    }
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    <FiEye
+                      color={iconColor}
+                      className="setting-form-password-icon"
+                    />
+                  </button>
+                ) : (
+                  <button
+                    className="setting-form-password-button"
+                    type="button"
+                    onClick={() =>
+                      setShowRepeatNewPassword(!showRepeatNewPassword)
+                    }
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    <FiEyeOff
+                      color={iconColor}
+                      className="setting-form-password-icon"
+                    />
+                  </button>
+                )}
+                {formik.touched.repeatNewPassword &&
+                  formik.errors.repeatNewPassword && (
+                    <p className="setting-form-input-error">
+                      {formik.errors.repeatNewPassword}
+                    </p>
+                  )}
+              </span>
+            </label>
+          </div>
         </div>
-        <button type="submit">Save</button>
+        <button type="submit" className="setting-form-submit">
+          Save
+        </button>
       </form>
     </SettingModalStyled>
   );

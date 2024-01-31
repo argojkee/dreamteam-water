76eeefe38cb1facec685c28dcdb587b66036bb95
argojkee <maksymhaidabura@gmail.com>
2024-01-31 22:57:46 +0200

    Update WaterRatioPanel.jsx

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,6 +1,12 @@
-import { CiCirclePlus } from "react-icons/ci";
+import { CiCirclePlus } from 'react-icons/ci';
 
-import { AddWaterButton, ButtonIcon, PanelDiv, SliderContainerDiv, SliderInput } from "./WaterRatioPanel.styled";
+import {
+  AddWaterButton,
+  ButtonIcon,
+  PanelDiv,
+  SliderContainerDiv,
+  SliderInput,
+} from './WaterRatioPanel.styled';
 
 // const marks = {
 //    0: '0%',
@@ -9,46 +15,46 @@ import { AddWaterButton, ButtonIcon, PanelDiv, SliderContainerDiv, SliderInput }
 // }
 
 export const WaterRatioPanel = () => {
-   // const [value, setValue] = useState(20); // добавить значение
-   const value = 20;
+  // const [value, setValue] = useState(20); // добавить значение
+  //   const value = 20;
 
-   // const handleSliderChange = (event) => {
-   //    setValue(event.target.value);
-   // };
+  // const handleSliderChange = (event) => {
+  //    setValue(event.target.value);
+  // };
 
-   return (
-      <PanelDiv>
-         <AddWaterButton>
-            <ButtonIcon>
-               <CiCirclePlus />
-            </ButtonIcon>
-            Add Water
-         </AddWaterButton>
-         <SliderContainerDiv className="slider-container">
-            {/* <Slider2
+  return (
+    <PanelDiv>
+      <AddWaterButton>
+        <ButtonIcon>
+          <CiCirclePlus />
+        </ButtonIcon>
+        Add Water
+      </AddWaterButton>
+      <SliderContainerDiv className="slider-container">
+        {/* <Slider2
                marks={marks}
                min={0}
                max={100}
                onChange={handleSliderChange}
                value={value}
             /> */}
-            <SliderInput 
-               type="range"
-               min="0"
-               max="100"
-               step='1'
-               // disabled
-               id="range-slider"
-               value={value}
-               // onChange={handleSliderChange}
-            />
-            
-            {/* <SliderValueDiv className="slider-value" id="sliderValue">
+        <SliderInput
+          type="range"
+          min="0"
+          max="100"
+          step="1"
+          // disabled
+          id="range-slider"
+          // value={value}
+          // onChange={handleSliderChange}
+        />
+
+        {/* <SliderValueDiv className="slider-value" id="sliderValue">
                {value}
             </SliderValueDiv> */}
-         </SliderContainerDiv>
-      </PanelDiv>
-   );
+      </SliderContainerDiv>
+    </PanelDiv>
+  );
 };
 
 export default WaterRatioPanel;

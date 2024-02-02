2586986eaede79e6588a6e2016161f109bd80c29
argojkee <maksymhaidabura@gmail.com>
2024-02-02 22:11:59 +0200

    Update Bottle.js

--- src/components/AuthForm/Bottle/Bottle.js
+++ src/components/AuthForm/Bottle/Bottle.js
@@ -1,80 +1,72 @@
 import { useState, useEffect, useRef } from 'react';
 import { useTransition, animated } from '@react-spring/web';
 
-import {ReactComponent as BottleCircle} from '../../../images/signIn-signUp/bottle/bottleCircle.svg';
+import { ReactComponent as BottleCircle } from '../../../images/signIn-signUp/bottle/bottleCircle.svg';
 
 /* styles import */
 import { BottleStyles } from './BottleStyles.styled';
 /* end */
 
 const Bottle = () => {
-
   const [parameters, setParameters] = useState([]);
   const [bottleStartY, setBottleStartY] = useState(0);
 
   const bottleRef = useRef();
- 
   useEffect(() => {
-
     const random = () => {
-        
-        let bottleCenterX = 0;
+      let bottleCenterX = 0;
 
-        if(bottleRef.current !== null) {
+      if (bottleRef.current !== null) {
+        bottleCenterX = bottleRef.current.offsetWidth / 2;
+        setBottleStartY(bottleRef.current.offsetHeight);
+      }
 
-          bottleCenterX = bottleRef.current.offsetWidth / 2;
-          setBottleStartY(bottleRef.current.offsetHeight);
-
-        } 
-        
-        return {
-   
-          size: randomGenerator(15, 5), 
-          x: randomGenerator(bottleCenterX + 20, bottleCenterX - 40),
-
-        };
+      return {
+        size: randomGenerator(15, 5),
+        x: randomGenerator(bottleCenterX + 20, bottleCenterX - 40),
       };
+    };
 
     const timer = setInterval(() => {
-
-        setParameters([...parameters, random()])
-       
+      setParameters([...parameters, random()]);
     }, randomGenerator(150, 50) * 10);
 
     return () => {
-        clearInterval(timer);
-    }; 
-    
-  }, []);
+      clearInterval(timer);
+    };
+  }, [parameters]);
 
   const randomGenerator = (max, min) => {
-
     return Math.round(Math.random() * (max - min) + min);
-    
   };
 
   const transitions = useTransition(parameters, {
-    from: {transform: `translateY(${bottleStartY - 20}px)`, opacity: '1',},
-    enter: {transform: `translateY(30px)`, opacity: '0,8',},
-    config: {duration: randomGenerator(4000, 2000), friction: randomGenerator(300, 5) * 10,}
-  })
+    from: { transform: `translateY(${bottleStartY - 20}px)`, opacity: '1' },
+    enter: { transform: `translateY(30px)`, opacity: '0,8' },
+    config: {
+      duration: randomGenerator(4000, 2000),
+      friction: randomGenerator(300, 5) * 10,
+    },
+  });
 
-  return ( 
-    
+  return (
     <BottleStyles>
-      <div className='bottleContainer' ref={bottleRef}>
-      { transitions((style, item) => 
-        (<animated.div style={style}>
-        
-          <BottleCircle style={{ position: 'absolute', width: `${item.size}px`, height: `${item.size}px`,
-          left: `${item.x}px`}}/>
-          
-        </animated.div>)
-        )}
+      <div className="bottleContainer" ref={bottleRef}>
+        {transitions((style, item) => (
+          <animated.div style={style}>
+            <BottleCircle
+              style={{
+                position: 'absolute',
+                width: `${item.size}px`,
+                height: `${item.size}px`,
+                left: `${item.x}px`,
+              }}
+            />
+          </animated.div>
+        ))}
       </div>
     </BottleStyles>
-
-  )
+  );
 };
 
 export default Bottle;

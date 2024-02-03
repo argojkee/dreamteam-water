c7bf8be9aff861ff0eb8f2ac424cea3271d2974b
ExLuZiVe53 <imstudent27@gmail.com>
2024-02-03 14:00:37 +0100

    added styles modal DailyNorma and BLL

--- src/components/EditNormaModal/EditNormaModal.jsx
+++ src/components/EditNormaModal/EditNormaModal.jsx
@@ -4,26 +4,26 @@ const EditNormaModal = () => {
   return (
     <StyledDailyNormaContainer>
       <h1>My daily norma</h1>
-      <form action="#" name="save_form">
-        <ul className="list">
-          <li className="link">
-            <p className="form-text">
-              For girl: <span className="formula"> V=(M*0.03) + (T*0.4)</span>
-            </p>
-          </li>
-          <li className="link">
-            <p className="form-text">
-              For man: <span className="formula"> V=(M*0,04) + (T*0,6)</span>
-            </p>
-          </li>
-        </ul>
+      <ul className="list">
+        <li className="link">
+          <p className="form-text">
+            For girl: <span className="formula"> V=(M*0.03) + (T*0.4)</span>
+          </p>
+        </li>
+        <li className="link">
+          <p className="form-text">
+            For man: <span className="formula"> V=(M*0,04) + (T*0,6)</span>
+          </p>
+        </li>
+      </ul>
 
-        <p className="invisibleText">
-          <span>&#42;</span>V is the volume of the water norm in liters per day,
-          M is your body weight, T is the time of active sports, or another type
-          of activity commensurate in terms of loads (in the absence of these,
-          you must set 0)
-        </p>
+      <p className="invisibleText">
+        <span>&#42;</span>V is the volume of the water norm in liters per day, M
+        is your body weight, T is the time of active sports, or another type of
+        activity commensurate in terms of loads (in the absence of these, you
+        must set 0)
+      </p>
+      <form action="#" name="save_form">
         <h2>Calculate your rate:</h2>
 
         <label htmlFor="#" className="form-text">
@@ -34,14 +34,26 @@ const EditNormaModal = () => {
           <input type="radio" name="gender" value="man" />
           For man
         </label>
-
+        <br />
         <label htmlFor="#" className="form-text">
           Your weight in kilograms:
           <input
-            type="number"
-            name="number"
+            type="text"
+            name="numberKilo"
             // value="0"
             className="numberKilo"
+          />
+        </label>
+        <p>
+          The time of active participation in sports or other activities with a
+          high physical. load in hours:
+        </p>
+        <label htmlFor="#" className="form-text time">
+          <input
+            type="text"
+            name="amountOfTime"
+            className="numberKilo"
+            // value=0
           />
         </label>
         <p className="form-text">
@@ -51,7 +63,7 @@ const EditNormaModal = () => {
 
         <h2 className="water">Write down how much water you will drink:</h2>
         <input
-          type="number"
+          type="text"
           name="number"
           // value="0"
           className="numberKilo"

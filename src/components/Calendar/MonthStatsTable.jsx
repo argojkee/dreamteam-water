e1a6df0b5fc068c3959f9495bacfffe04b518d3a
Sv-L <121663409+Sv-L@users.noreply.github.com>
2024-02-07 08:22:19 +0100

    added a start restriction on the date of registration

--- src/components/Calendar/MonthStatsTable.jsx
+++ src/components/Calendar/MonthStatsTable.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import MonthSwitcher from './MonthSwitcher/MonthSwitcher';
 import MonthStatistic from './MonthStatistic/MonthStatistic';
-import { compareDates, today } from './helpers/getDate';
+import { compareDates, funcGetDate, today } from './helpers/getDate';
 import { useSelector } from 'react-redux';
 import {
   getCurrentDay,
@@ -13,6 +13,7 @@ import { updateOrAddCurrentDay } from './helpers/updateOrAddCurrentDay';
 import { PiSpinnerGap } from 'react-icons/pi';
 import { SpinnerContainer } from './SpinnerContainer.styled';
 import { RelativDiv } from './MonthStatsTablet.styled';
+import { getStartDay } from '../../redux/auth/authSelectors';
 
 const MonthStatsTable = () => {
   const [selectedMonth, setSelectedMonth] = useState({ ...today });
@@ -21,6 +22,8 @@ const MonthStatsTable = () => {
   const currentDayStatistic = useSelector(getCurrentDay);
   const isMonthLoading = useSelector(getIsMonthDataLoading);
   const [isOtherMonthLoading, setIsOtherMonthLoading] = useState(false);
+  const startDay = useSelector(getStartDay);
+  const registrationDate = funcGetDate(startDay);
 
   useEffect(() => {
     const picData = [selectedMonth.month, selectedMonth.year];
@@ -72,6 +75,7 @@ const MonthStatsTable = () => {
       <MonthSwitcher
         selectedMonth={selectedMonth}
         setSelectedMonth={setSelectedMonth}
+        registrationDate={registrationDate}
       />{' '}
       <RelativDiv>
         {isMonthLoading || isOtherMonthLoading ? (
@@ -82,7 +86,8 @@ const MonthStatsTable = () => {
           <MonthStatistic
             selectedMonth={selectedMonth}
             monthStatistic={monthStatistic}
-            setSelectedMonth={setSelectedMonth}
+              setSelectedMonth={setSelectedMonth}
+              registrationDate={registrationDate}
           />
         )}{' '}
       </RelativDiv>

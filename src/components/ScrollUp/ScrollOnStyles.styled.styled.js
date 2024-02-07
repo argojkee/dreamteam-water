74ef54120dd095c52654727520995d5bf9151ae3
DmShko <vombat_schev@i.ua>
2024-02-07 19:08:35 +0200

    signIn-signUp-22

---
+++ src/components/ScrollUp/ScrollOnStyles.styled.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const DailyNormStyles = styled.div`
+
+  position: absolute;
+
+  right: 70px;
+
+  width: '30px;
+  height: '30px;
+  
+`;
--- src/components/ScrollUp/ScrollUp.jsx
+++ src/components/ScrollUp/ScrollUp.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { ReactComponent as LetterDown } from '../../images/scrollUp/drop-svgrepo-com.svg'
+import { ScrollOnStyles } from './ScrollOnStyles.styled';
 
 const scrollUp = () => {
+
+  const scrollOn = () => {
+    
+  };
+
   return (
-    <div>scrollUp</div>
+    <ScrollOnStyles $isDark={isDark}>
+
+        <div onClick={scrollOn} style={windows ? {backgroundColor: 'lightgray', animationIterationCount: 0, visibility: 'hidden'}: selectorGallerySlice.messagesBuffer !== undefined && selectorGallerySlice.mesBuffLength === selectorGallerySlice.messagesBuffer.length ? {backgroundColor: 'lightgray', animationIterationCount: 0, visibility: 'visible'} : {backgroundColor: 'rgba(194, 212, 31, 0.801)'}}>
+            <LetterDown style={{width:'35px', height:'35px'}}/>
+        </div>
+
+    </ScrollOnStyles >
   )
 }
 
 export default scrollUp
+
